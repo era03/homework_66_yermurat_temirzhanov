711a7b90fe4a9b8f62c7885ac508e633985e435f
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-07 23:08:37 +0600

    Выполнено задание №1

---
+++ Task-1/js/main.js
@@ -0,0 +1,13 @@
+const sentence1 = document.getElementById('string-3')
+const sentence2 = document.getElementById('string-5')
+const sentence3 = document.getElementById('string-2')
+const sentence4 = document.getElementById('string-5')
+const sentence5 = document.getElementById('string-4')
+const sentence6 = document.getElementById('string-1')
+
+console.log(sentence1.textContent)
+console.log(sentence2.textContent)
+console.log(sentence3.textContent)
+console.log(sentence4.textContent)
+console.log(sentence5.textContent)
+console.log(sentence6.textContent)
