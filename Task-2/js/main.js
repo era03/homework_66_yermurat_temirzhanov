9915550e3737292665f030aecd1489fdc59c0eb3
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-07 23:18:50 +0600

    Выполнено задание 2, первые 3 элемента покрашены в красный цвет, последние 3 в зеленый с помощью JS

---
+++ Task-2/js/main.js
@@ -0,0 +1,8 @@
+const divs = document.getElementsByTagName('div')
+
+divs[0].style.color = 'red'
+divs[1].style.color = 'red'
+divs[2].style.color = 'red'
+divs[3].style.color = 'green'
+divs[4].style.color = 'green'
+divs[5].style.color = 'green'
